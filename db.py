cdcd7e6f3a11bff642bce3d0187f41f7801df299
sanil jain <jainsanil18@gmail.com>
2020-05-11 07:58:40 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
+from config import Config
 #connect the sqlalchemy framework to the db
 engine = create_engine(Config.POSTGRES_URL, echo = True)
 Base = declarative_base()
